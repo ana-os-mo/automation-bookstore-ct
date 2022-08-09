e28fb3d335f27519e421a549f748721e424d1751
Ana <anmosoriomo@unal.edu.co>
2022-08-08 23:42:13 -0500

    adding tests

---
+++ e2e/cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    video: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
+
+
+require('@applitools/eyes-cypress')(module);
---
+++ e2e/cypress/e2e/search.cy.js
@@ -0,0 +1,31 @@
+describe("Search for books", () => {
+    beforeEach(() => {
+        cy.visit("https://ctbookstore.azurewebsites.net/")
+    });
+
+    it("should return one book with title Agile Testing", () => {
+        const title = "Agile Testing";
+        const count = 1;
+
+        cy.get("#searchBar").type(title, {delay: 200});
+        cy.get("li:not(.ui-screen-hidden)").should("have.length", count, `There should be exactly ${count} book(s) visible`);
+        cy.get("h2").should("contain.text", title, `${title} should be visible`);
+    });
+
+    it("should return multiple books with title Test", () => {
+        const title = "Test";
+        const expectedBooks = [
+            "Test Automation in the Real World",
+            "Experiences of Test Automation",
+            "Agile Testing",
+            "How Google Tests Software",
+            "Java For Testers"];
+        const count = expectedBooks.length;
+
+        cy.get("#searchBar").type(title, {delay: 200});
+        cy.get("li:not(.ui-screen-hidden)")
+        .should("have.length", count, `There should be exactly ${count} book(s) visible`);
+        expectedBooks.forEach(b => cy.get("h2")
+        .should("contain.text", b, `${b} should be visible`));
+    });
+});
